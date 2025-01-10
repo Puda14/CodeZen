2131b0c923dcc48aa35a1e36ad7f8dd6ccbc7281
Puda14 <pdathp14@gmail.com>
2025-01-11 00:33:53 +0700

    ui: execute_code

---
+++ frontend/src/tmp/testcase2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;  
+  int arr[2];  
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+  int max_val = arr[0];
+  int min_val = arr[0];
+  for (int i = 1; i < n; ++i) {
+    if (arr[i] > max_val) max_val = arr[i];
+    if (arr[i] < min_val) min_val = arr[i];
+  }
+  cout << max_val << " " << min_val << endl;
+  return 0;
+}
